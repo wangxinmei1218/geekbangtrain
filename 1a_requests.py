88e5ecff689e021d9a34e4e02a22285feeb655d1
wangxinmei1218 <wang-xm@waocal.com.cn>
2020-07-21 09:05:31 +0800

    增加头函数

--- 1a_requests.py
+++ 1a_requests.py
@@ -2,6 +2,12 @@
 
 import requests
 
+from fake_useragent import UserAgent      # fake_useragent模块 user-agent的获取 
+
+
+ua = UserAgent()        # 实例化，实例化时需要联网但是网站不太稳定 
+
+
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 
 header = {'user-agent':user_agent}
